aa7ec91c0a83704a1662aa12a3aea52107140635
Ray Kiddy <ray@ganymede.org>
2022-11-16 13:15:45 -0800

    first app for displaying imports of tables, what failed.

---
+++ imports.py
@@ -0,0 +1,24 @@
+
+import sys
+
+from flask import Flask
+from jinja2 import Environment, PackageLoader
+
+# sys.path.append("/home/ray/opinions/")
+import data_imports
+
+imports = Flask(__name__)
+application = imports
+env = Environment(loader=PackageLoader('imports', 'pages'))
+
+
+@imports.route('/imports')
+def list_imports():
+    main = env.get_template('imports.html')
+    context = data_imports.build('imports')
+    return main.render(**context)
+
+
+if __name__ == '__main__':
+    imports.run()
+
