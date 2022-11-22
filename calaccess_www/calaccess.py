a362497bb3a66c78dc441f3900c8b8dab5baecd7
Ray Kiddy <ray@ganymede.org>
2022-11-22 15:00:17 -0800

    Split up the build method in data to better parameterize.

--- calaccess_www/calaccess.py
+++ calaccess_www/calaccess.py
@@ -18,35 +18,35 @@ env = Environment(loader=PackageLoader('calaccess', 'pages'))
 @calaccess.route(f"/{cfg['WWW']}")
 def contracts_front():
     main = env.get_template('calaccess.html')
-    context = data.build('calaccess_front')
+    context = data.calacess_front()
     return main.render(**context)
 
 
-@calaccess.route(f"/{cfg['WWW']}filingdate/<param>")
-def contracts_filingdate(param):
+@calaccess.route(f"/{cfg['WWW']}filingdate/<filing_date>")
+def contracts_filingdate(filing_date):
     main = env.get_template('calaccess_filingdate.html')
-    context = data.build('calaccess_filingdate', param)
+    context = data.calaccess_filing_date(filing_date)
     return main.render(**context)
 
 
-@calaccess.route(f"/{cfg['WWW']}filingdate/<param>/form_id/<param2>")
-def contracts_filingdate_form_id(param, param2):
+@calaccess.route(f"/{cfg['WWW']}filingdate/<filing_date>/form_id/<form_id>")
+def contracts_filingdate_form_id(filing_date, form_id):
     main = env.get_template('calaccess_filingdate.html')
-    context = data.build('calaccess_filingdate', param, 'form_id', param2)
+    context = data.calaccess_filing_date(filing_date, form_id)
     return main.render(**context)
 
 
-@calaccess.route(f"/{cfg['WWW']}filing_raw/<param>")
-def contracts_filing_raw(param):
+@calaccess.route(f"/{cfg['WWW']}filing_raw/<filing_id>")
+def contracts_filing_raw(filing_id):
     main = env.get_template('calaccess_filing_raw.html')
-    context = data.build('calaccess_filing_raw', param)
+    context = data.filing_raw(filing_id)
     return main.render(**context)
 
 
-@calaccess.route(f"/{cfg['WWW']}filer_raw/<param>")
-def contracts_filer_raw(param):
+@calaccess.route(f"/{cfg['WWW']}filer_raw/<filer_id>")
+def contracts_filer_raw(filer_id):
     main = env.get_template('calaccess_filer_raw.html')
-    context = data.build('calaccess_filer_raw', param)
+    context = data.filer_raw(filer_id)
     return main.render(**context)
 
 
--- calaccess_www/data.py
+++ calaccess_www/data.py
@@ -48,291 +48,294 @@ def full_name(naml, namf, namt, nams):
     return buf
 
 
-def build(param, extra=None, param2=None, extra2=None ):
+def calacess_front(start_date = None, end_date = None):
+    context = dict()
+
+    sql = """
+    select date(filing_date) as filing_date, count(0) as count from filer_filings
+    where filing_date < '2030-01-01'
+    group by filing_date order by filing_date desc limit 30
+    """
+
+    rows = conn.execute(sql).fetchall()
+    next_rows = list()
+    for row in rows:
+        next_rows.append(
+            {
+                'filing_date': row['filing_date'].strftime('%Y-%m-%d'),
+                'count': row['count']
+            }
+        )
+
+    lowest_date = min([r['filing_date'] for r in next_rows])
+
+    sql = f"""
+    select filing_date, filing_id, form_id
+    from filer_filings
+    where filing_date >= '{lowest_date}' and
+        filing_date < '2030-01-01'
+    """
+
+    cols = {
+        'filing_date': 0,
+        'filing_id': 1,
+        'form_id': 2
+    }
+
+    rows = conn.execute(sql).fetchall()
+    data = common.fill_in_table(rows, cols)
+
+    # filings -> [filing_date] -> [form_id] -> # for the filing_date
+    # totals -> [filing_date] -> # for the all forms for the filing_date
+
+    filings = dict()
+    all_form_ids = list()
+    totals = dict()
+
+    for datum in data:
+
+        all_form_ids.append(datum['form_id'])
+        filing_date = datum['filing_date'].strftime('%Y-%m-%d')
+
+        if filing_date not in filings:
+            filings[filing_date] = dict()
+        if filing_date not in totals:
+            totals[filing_date] = 0
+
+        totals[filing_date] += 1
+        if datum['form_id'] not in filings[filing_date]:
+            filings[filing_date][datum['form_id']] = 1
+        else:
+            filings[filing_date][datum['form_id']] += 1
+
+    all_form_ids = sorted(list(set(all_form_ids)))
+
+    for filing_date in filings:
+        for form_id in all_form_ids:
+            if form_id not in filings[filing_date]:
+                filings[filing_date][form_id] = 0
+
+    form_ids = dict()
+    form_ids['campaign'] = list()
+    form_ids['lobbyist'] = list()
+    form_ids['others'] = list()
+
+    for form_id in all_form_ids:
+        if form_id.startswith('F4'):
+            form_ids['campaign'].append(form_id)
+        elif form_id.startswith('F6'):
+            form_ids['lobbyist'].append(form_id)
+        else:
+            form_ids['others'].append(form_id)
+
+    filing_dates = sorted(list(filings.keys()))
+    filing_dates.reverse()
+
+    context['form_ids'] = form_ids
+    context['totals'] = totals
+    context['filing_dates'] = filing_dates
+    context['filings'] = filings
+
+    return context
+
+
+def calaccess_filing_date(filing_date, form_id=None):
 
     context = dict()
 
-    if param == 'calaccess_front':
-
-        sql = """
-        select date(filing_date) as filing_date, count(0) as count from filer_filings
-        where filing_date < '2030-01-01'
-        group by filing_date order by filing_date desc limit 30
-        """
-
-        rows = conn.execute(sql).fetchall()
-        next_rows = list()
-        for row in rows:
-            next_rows.append(
-                {
-                    'filing_date': row['filing_date'].strftime('%Y-%m-%d'),
-                    'count': row['count']
-                }
-            )
-
-        lowest_date = min([r['filing_date'] for r in next_rows])
-
+    if form_id is None:
         sql = f"""
-        select filing_date, filing_id, form_id
-        from filer_filings
-        where filing_date >= '{lowest_date}' and
-            filing_date < '2030-01-01'
+        select f1.filing_id, f1.filer_id, f1.period_id, f1.form_id,
+            f1.filing_sequence as filing_seq, date(f1.rpt_start) as rpt_start,
+            date(f1.rpt_end) as rpt_end, f2.filer_type, f2.naml, f2.namf, f2.namt,
+            f2.nams, f2.city, f2.st, f2.zip4, f2.effect_dt, f3.start_date, f3.period_desc
+        from filer_filings f1 left join filername f2 on f1.filer_id = f2.filer_id
+            left join filing_period f3 on f1.period_id = f3.period_id
+                where f1.filing_date = '{filing_date}'
+        """
+    else:
+        sql = f"""
+        select f1.filing_id, f1.filer_id, f1.period_id, f1.form_id,
+            f1.filing_sequence as filing_seq, date(f1.rpt_start) as rpt_start,
+            date(f1.rpt_end) as rpt_end, f2.filer_type, f2.naml, f2.namf, f2.namt,
+            f2.nams, f2.city, f2.st, f2.zip4, f2.effect_dt, f3.start_date, f3.period_desc
+        from filer_filings f1 left join filername f2 on f1.filer_id = f2.filer_id
+            left join filing_period f3 on f1.period_id = f3.period_id
+                where f1.filing_date = '{filing_date}' and
+                    f1.form_id = '{form_id}'
         """
 
-        cols = {
-            'filing_date': 0,
-            'filing_id': 1,
-            'form_id': 2
-        }
+    rows = conn.execute(sql).fetchall()
 
-        rows = conn.execute(sql).fetchall()
-        data = common.fill_in_table(rows, cols)
+    cols = {
+        'filing_id': 0,
+        'filer_id': 1,
+        'period_id': 2,
+        'form_id': 3,
+        'filing_seq': 4,
+        'rpt_start': 5,
+        'rpt_end': 6,
+        'filer_type': 7,
+        'naml': 8,
+        'namf': 9,
+        'namt': 10,
+        'nams': 11,
+        'city': 12,
+        'st': 13,
+        'zip4': 14,
+        'effect_dt': 15,
+        'period_start': 16,
+        'period_desc': 17
+    }
 
-        # filings -> [filing_date] -> [form_id] -> # for the filing_date
-        # totals -> [filing_date] -> # for the all forms for the filing_date
+    data = common.fill_in_table(rows, cols)
 
-        filings = dict()
-        all_form_ids = list()
-        totals = dict()
+    next_data = dict()
 
-        for datum in data:
+    for datum in data:
+        filer_id = datum['filer_id']
 
-            all_form_ids.append(datum['form_id'])
-            filing_date = datum['filing_date'].strftime('%Y-%m-%d')
-
-            if filing_date not in filings:
-                filings[filing_date] = dict()
-            if filing_date not in totals:
-                totals[filing_date] = 0
-
-            totals[filing_date] += 1
-            if datum['form_id'] not in filings[filing_date]:
-                filings[filing_date][datum['form_id']] = 1
-            else:
-                filings[filing_date][datum['form_id']] += 1
-
-        all_form_ids = sorted(list(set(all_form_ids)))
-
-        for filing_date in filings:
-            for form_id in all_form_ids:
-                if form_id not in filings[filing_date]:
-                    filings[filing_date][form_id] = 0
-
-        form_ids = dict()
-        form_ids['campaign'] = list()
-        form_ids['lobbyist'] = list()
-        form_ids['others'] = list()
-
-        for form_id in all_form_ids:
-            if form_id.startswith('F4'):
-                form_ids['campaign'].append(form_id)
-            elif form_id.startswith('F6'):
-                form_ids['lobbyist'].append(form_id)
-            else:
-                form_ids['others'].append(form_id)
-
-        filing_dates = sorted(list(filings.keys()))
-        filing_dates.reverse()
-
-        context['form_ids'] = form_ids
-        context['totals'] = totals
-        context['filing_dates'] = filing_dates
-        context['filings'] = filings
-
-        return context
-
-    if param == 'calaccess_filingdate':
-
-        if extra2 is None:
-            sql = f"""
-            select f1.filing_id, f1.filer_id, f1.period_id, f1.form_id,
-                f1.filing_sequence as filing_seq, date(f1.rpt_start) as rpt_start,
-                date(f1.rpt_end) as rpt_end, f2.filer_type, f2.naml, f2.namf, f2.namt,
-                f2.nams, f2.city, f2.st, f2.zip4, f2.effect_dt, f3.start_date, f3.period_desc
-            from filer_filings f1 left join filername f2 on f1.filer_id = f2.filer_id
-                left join filing_period f3 on f1.period_id = f3.period_id
-                    where f1.filing_date = '{extra}'
-            """
+        if filer_id not in next_data:
+            next_data[filer_id] = datum
         else:
-            sql = f"""
-            select f1.filing_id, f1.filer_id, f1.period_id, f1.form_id,
-                f1.filing_sequence as filing_seq, date(f1.rpt_start) as rpt_start,
-                date(f1.rpt_end) as rpt_end, f2.filer_type, f2.naml, f2.namf, f2.namt,
-                f2.nams, f2.city, f2.st, f2.zip4, f2.effect_dt, f3.start_date, f3.period_desc
-            from filer_filings f1 left join filername f2 on f1.filer_id = f2.filer_id
-                left join filing_period f3 on f1.period_id = f3.period_id
-                    where f1.filing_date = '{extra}' and
-                        f1.form_id = '{extra2}'
-            """
+            effect_dt = datum['effect_dt']
+            if effect_dt > next_data[filer_id]['effect_dt']:
+                next_data[filer_id] = datum
+
+    for filer_id in next_data:
+        entry = next_data[filer_id]
+        entry['full_name'] = full_name(entry['naml'], entry['namf'], entry['namt'], entry['nams'])
+
+        # either the period_start or desc might be NULL, or both.
+        if entry['period_start'] is not None and entry['period_desc'] is not None:
+            start_date = entry['period_start'].strftime("%Y")
+            entry['period'] = f"{start_date}&nbsp;Q{entry['period_desc'][-1]}"
+        if entry['period_start'] is not None and entry['period_desc'] is None:
+            start_date = entry['period_start'].strftime("%Y")
+            entry['period'] = f"{start_date}&nbsp;Q?"
+        if entry['period_start'] is None and entry['period_desc'] is not None:
+            entry['period'] = f"????&nbsp;Q{entry['period_desc'][-1]}"
+        if entry['period_start'] is None and entry['period_desc'] is None:
+            entry['period'] = None
+
+    context['filing_date'] = filing_date
+    context['form_id'] = form_id
+
+    context['calaccess_filing_date'] = common.order_dicts_by_key(list(next_data.values()), 'filing_id')
+
+    return context
+
+
+def filing_raw(filing_id):
+
+    context = dict()
+
+    amend_id = 0
+
+    context['filing_id'] = filing_id
+
+    url_start = "https://cal-access.sos.ca.gov/PDFGen/pdfgen.prg"
+
+    tables = list()
+
+    for table in tables_with_column('filing_id'):
+
+        sql = f"desc {table}"
 
         rows = conn.execute(sql).fetchall()
+        column_names = list()
+        sideways = dict()
+        cols = dict()
+        idx = 0
 
-        cols = {
-            'filing_id': 0,
-            'filer_id': 1,
-            'period_id': 2,
-            'form_id': 3,
-            'filing_seq': 4,
-            'rpt_start': 5,
-            'rpt_end': 6,
-            'filer_type': 7,
-            'naml': 8,
-            'namf': 9,
-            'namt': 10,
-            'nams': 11,
-            'city': 12,
-            'st': 13,
-            'zip4': 14,
-            'effect_dt': 15,
-            'period_start': 16,
-            'period_desc': 17
-        }
+        for row in rows:
+            column_names.append(row[0])
+            cols[row[0]] = idx
+            sideways[row[0]] = list()
+            idx += 1
 
+        sql = f"select * from {table} where filing_id = {filing_id}"
+
+        rows = conn.execute(sql).fetchall()
         data = common.fill_in_table(rows, cols)
 
-        next_data = dict()
-
-        for datum in data:
-            filer_id = datum['filer_id']
-
-            if filer_id not in next_data:
-                next_data[filer_id] = datum
-            else:
-                effect_dt = datum['effect_dt']
-                if effect_dt > next_data[filer_id]['effect_dt']:
-                    next_data[filer_id] = datum
-
-        for filer_id in next_data:
-            entry = next_data[filer_id]
-            entry['full_name'] = full_name(entry['naml'], entry['namf'], entry['namt'], entry['nams'])
-
-            # either the period_start or desc might be NULL, or both.
-            if entry['period_start'] is not None and entry['period_desc'] is not None:
-                start_date = entry['period_start'].strftime("%Y")
-                entry['period'] = f"{start_date}&nbsp;Q{entry['period_desc'][-1]}"
-            if entry['period_start'] is not None and entry['period_desc'] is None:
-                start_date = entry['period_start'].strftime("%Y")
-                entry['period'] = f"{start_date}&nbsp;Q?"
-            if entry['period_start'] is None and entry['period_desc'] is not None:
-                entry['period'] = f"????&nbsp;Q{entry['period_desc'][-1]}"
-            if entry['period_start'] is None and entry['period_desc'] is None:
-                entry['period'] = None
-
-        context['filing_date'] = extra
-        context['form_id'] = extra2
-
-        context['calaccess_filing_date'] = common.order_dicts_by_key(list(next_data.values()), 'filing_id')
-
-        return context
-
-    if param == 'calaccess_filing_raw':
-
-        filing_id = extra
-        amend_id = 0
-
-        context['filing_id'] = filing_id
-
-        url_start = "https://cal-access.sos.ca.gov/PDFGen/pdfgen.prg"
-
-        tables = list()
-
-        for table in tables_with_column('filing_id'):
-
-            sql = f"desc {table}"
-
-            rows = conn.execute(sql).fetchall()
-            column_names = list()
-            sideways = dict()
-            cols = dict()
-            idx = 0
-
-            for row in rows:
-                column_names.append(row[0])
-                cols[row[0]] = idx
-                sideways[row[0]] = list()
-                idx += 1
-
-            sql = f"select * from {table} where filing_id = {filing_id}"
-
-            rows = conn.execute(sql).fetchall()
-            data = common.fill_in_table(rows, cols)
-
-            if len(data) > 0:
-                for datum in data:
-                    for col_name in column_names:
-                        sideways[col_name].append(datum[col_name])
-
-                if 'amend_id' in sideways and len(sideways['amend_id']) > 0:
-                    amend_id = max(sideways['amend_id'])
-
-                buf = f"<table border=\"1\">\n    <caption>Table: {table}</caption>\n"
-
+        if len(data) > 0:
+            for datum in data:
                 for col_name in column_names:
-                    buf = f"{buf}    <tr>\n        <th>{col_name}</th>\n"
+                    sideways[col_name].append(datum[col_name])
 
-                    for value in sideways[col_name]:
-                        buf = f"{buf}        <td>&nbsp;&nbsp;</td><td>{value}</td>\n"
+            if 'amend_id' in sideways and len(sideways['amend_id']) > 0:
+                amend_id = max(sideways['amend_id'])
 
-                    buf = f"{buf}    </tr>"
+            buf = f"<table border=\"1\">\n    <caption>Table: {table}</caption>\n"
 
-                buf = f"{buf}</table>\n"
+            for col_name in column_names:
+                buf = f"{buf}    <tr>\n        <th>{col_name}</th>\n"
 
-                tables.append(buf)
+                for value in sideways[col_name]:
+                    buf = f"{buf}        <td>&nbsp;&nbsp;</td><td>{value}</td>\n"
 
-        context['pdf'] = f"{url_start}?filingid={filing_id}&amendid={amend_id}"
+                buf = f"{buf}    </tr>"
 
-        context['tables'] = tables
+            buf = f"{buf}</table>\n"
 
-        return context
+            tables.append(buf)
 
-    if param == 'calaccess_filer_raw':
+    context['pdf'] = f"{url_start}?filingid={filing_id}&amendid={amend_id}"
 
-        filer_id = extra
+    context['tables'] = tables
 
-        context['filer_id'] = filer_id
+    return context
 
-        tables = list()
 
-        for table in tables_with_column('filer_id'):
+def filer_raw(filer_id):
 
-            sql = f"desc {table}"
+    context = dict()
 
-            rows = conn.execute(sql).fetchall()
-            column_names = list()
-            sideways = dict()
-            cols = dict()
-            idx = 0
+    context['filer_id'] = filer_id
 
-            for row in rows:
-                column_names.append(row[0])
-                cols[row[0]] = idx
-                sideways[row[0]] = list()
-                idx += 1
+    tables = list()
 
-            sql = f"select * from {table} where filer_id = {filer_id}"
+    for table in tables_with_column('filer_id'):
 
-            rows = conn.execute(sql).fetchall()
-            data = common.fill_in_table(rows, cols)
+        sql = f"desc {table}"
 
-            if len(data) > 0:
-                for datum in data:
-                    for col_name in column_names:
-                        sideways[col_name].append(datum[col_name])
+        rows = conn.execute(sql).fetchall()
+        column_names = list()
+        sideways = dict()
+        cols = dict()
+        idx = 0
 
-                buf = f"<table border=\"1\">\n    <caption>Table: {table}</caption>\n"
+        for row in rows:
+            column_names.append(row[0])
+            cols[row[0]] = idx
+            sideways[row[0]] = list()
+            idx += 1
 
+        sql = f"select * from {table} where filer_id = {filer_id}"
+
+        rows = conn.execute(sql).fetchall()
+        data = common.fill_in_table(rows, cols)
+
+        if len(data) > 0:
+            for datum in data:
                 for col_name in column_names:
-                    buf = f"{buf}    <tr>\n        <th>{col_name}</th>\n"
+                    sideways[col_name].append(datum[col_name])
 
-                    for value in sideways[col_name]:
-                        buf = f"{buf}        <td>&nbsp;&nbsp;</td><td>{value}</td>\n"
+            buf = f"<table border=\"1\">\n    <caption>Table: {table}</caption>\n"
 
-                    buf = f"{buf}    </tr>"
+            for col_name in column_names:
+                buf = f"{buf}    <tr>\n        <th>{col_name}</th>\n"
 
-                buf = f"{buf}</table>\n"
+                for value in sideways[col_name]:
+                    buf = f"{buf}        <td>&nbsp;&nbsp;</td><td>{value}</td>\n"
 
-                tables.append(buf)
+                buf = f"{buf}    </tr>"
 
-        context['tables'] = tables
+            buf = f"{buf}</table>\n"
 
-        return context
+            tables.append(buf)
+
+    context['tables'] = tables
+
+    return context
